453137edfabd60ff62150ca09ca30732f15436d2
Aayush Patel <aayush@Aayushs-MacBook-Air.local>
2021-05-14 15:00:09 +0530

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div class="form">
+
+          <div class="login">
+
+            <div class="login-header">
+              <h2>WELCOME TO E-CELL</h2>
+            </div>
+
+          </div>
+
+          <form class="login-form">
+
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>log in</button>
+
+          </form>
+
+        </div>
+
   );
 }
 
